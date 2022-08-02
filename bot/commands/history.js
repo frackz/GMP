6e8a2dc45d86875fde78bbf65b08444ca063c895
frede <65610875+dkfrede@users.noreply.github.com>
2022-08-02 19:36:40 +0200

    name history

--- bot/commands/history.js
+++ bot/commands/history.js
@@ -1,5 +1,5 @@
 //https://api.mojang.com/user/profiles/:uuid/names
-
+const axios = require('axios')
 module.exports = {
     data: {
         "name": "history",
@@ -15,6 +15,25 @@ module.exports = {
     },
     execute(client, interaction, config, db) {
         const username = interaction.options.getString('username');
-
+        axios.get('https://api.mojang.com/users/profiles/minecraft/'+username).then((res) => {
+            if (res.data == '') {return interaction.reply({content: "User does not exist.", ephemeral: true})}
+            axios.get('https://api.mojang.com/user/profiles/'+res.data.id+'/names').then((history) => {
+                if (history.data == null) {return interaction.reply({content: "Cannot find history", ephemeral: true})}
+                const data = history.data
+                var names = ''
+                for (var i = 0; i < data.length; i++) {
+                    const currentdata = data[i]
+                    if (!currentdata.changedToAt) {
+                        names=currentdata.name+'\n'
+                    } else {
+                        names=names+currentdata.name+' - '+new Date(currentdata.changedToAt).toLocaleDateString()+'\n'
+                    }
+                }
+                return interaction.reply({embeds:[{
+                    "title": "Name history...",
+                "description": "The user had all of these names...\n``"+names+"``",
+                "color": 16774400}], ephemeral: true})
+            }).catch((err) => console.error)
+        }).catch((err) => console.error)
     }
 }
